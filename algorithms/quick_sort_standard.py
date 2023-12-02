35b57a2fdc06c81543ad93bbd1f3874105212aa9
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-02 12:56:17 -0800

    Add quick sort partioning algo

--- algorithms/quick_sort_standard.py
+++ algorithms/quick_sort_standard.py
@@ -1,5 +1,18 @@
 import utilities.utils
 
 # Uses standard quick sort to sort input array
-def quick_sort_standard(array):
-    pass
+def quick_sort_standard(array, low, high, k):
+    if low <= high:
+        pivot_index = utilities.utils.partition(array, low, high)
+
+        # If pivot position == kth element
+        if pivot_index == k:
+            return array[pivot_index]
+        
+        # If pivot position > k, search left sub-array
+        elif pivot_index > k:
+            return quick_sort_standard(array, low, pivot_index - 1, k)
+        
+        # If pivot position < k, search right sub-array
+        else:
+            return quick_sort_standard(array, pivot_index + 1, high, k)
