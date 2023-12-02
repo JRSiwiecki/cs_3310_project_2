f9e972f9749faf16fbac9072a907d1acc1696939
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-01 17:17:43 -0800

    Add tests for merge_sort in main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello")
+from algorithms.merge_sort import kth_smallest_merge_sort
+
+test = [4, 3, 6, 1, 9, 10, 2, 5]
+low = 0
+high = len(test)
+
+kth_smallest_merge_sort(low, high, test)
+
+print(test)
