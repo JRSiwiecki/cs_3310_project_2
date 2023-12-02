f8f8567c8c56fab4021313e95a7f849950830b05
JRSiwiecki <JRSiwiecki@gmail.com>
2023-12-02 13:27:54 -0800

    Update main.py for constant variables

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ import algorithms.quick_sort_standard
 import utilities.utils
 
 # Parameters for random array generation
-array_length = 100
-min_value = 0
-max_value = 100_000
+ARRAY_LENGTH = 10
+MIN_VALUE = 0
+MAX_VALUE = ARRAY_LENGTH + 1
 
 # Generate a random array
-random_array = utilities.utils.generate_random_array(array_length, [min_value, max_value])
+random_array = utilities.utils.generate_random_array(ARRAY_LENGTH, [MIN_VALUE, MAX_VALUE])
 
 # Number of times to test each algorithm with each random array
-iterations = 5
+ITERATIONS = 5
 
 # Test for k
-k = 50
+K = 5
 
 # Switch to true to see intermediary unsorted -> sorted arrays
-display_output = False
+display_output = True
 
-set_results = {"array_length": array_length, "results": []}
+set_results = {"array_length": ARRAY_LENGTH, "results": []}
 
 # List of algorithms to test
 algorithm_list = [
@@ -29,7 +29,7 @@ algorithm_list = [
 
 # Test each algorithm on a given number of iterations while tracking time for execution
 for algorithm in algorithm_list:
-    print(f"----- {algorithm.__name__.upper()} for array_length = {array_length}, k = {k}  -----")
+    print(f"----- {algorithm.__name__.upper()} for array_length = {ARRAY_LENGTH}, k = {K}  -----")
 
     if display_output:
         # Display the unsorted array
@@ -37,16 +37,16 @@ for algorithm in algorithm_list:
     
     execution_times = []
 
-    for _ in range(iterations):   
-        _, execution_time = utilities.utils.time_algorithm(algorithm, random_array, k)
+    for _ in range(ITERATIONS):   
+        _, execution_time = utilities.utils.time_algorithm(algorithm, random_array, K)
         execution_times.append(execution_time)
 
         if display_output:
             # Display the sorted array
-            sorted_result, _ = utilities.utils.time_algorithm(algorithm, random_array, k)
+            sorted_result, _ = utilities.utils.time_algorithm(algorithm, random_array, K)
             print("Sorted Array:", sorted_result)
 
-    avg_execution_time = sum(execution_times) / iterations
+    avg_execution_time = sum(execution_times) / ITERATIONS
     set_results["results"].append({"algorithm": algorithm.__name__, 
                                    "avg_execution_time": avg_execution_time})
     print(f"Average Execution Time: {avg_execution_time:.6f} seconds")
